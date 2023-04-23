fcd0a3922f34f026de4f3152e379d29403e6fe25
nebulove <nebuloff@googlemail.com>
2023-04-24 01:21:38 +0400

    add AddEditNoteViewModel

--- app/src/main/java/com/nebulov/noteappmvvm/di/AppModule.kt
+++ app/src/main/java/com/nebulov/noteappmvvm/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.nebulov.noteappmvvm.feature_note.data.repository.NoteRepositoryImpl
 import com.nebulov.noteappmvvm.feature_note.domain.repository.NoteRepository
 import com.nebulov.noteappmvvm.feature_note.domain.use_case.AddNote
 import com.nebulov.noteappmvvm.feature_note.domain.use_case.DeleteNote
+import com.nebulov.noteappmvvm.feature_note.domain.use_case.GetNote
 import com.nebulov.noteappmvvm.feature_note.domain.use_case.GetNotes
 import com.nebulov.noteappmvvm.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -41,7 +42,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote((repository))
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
---
+++ app/src/main/java/com/nebulov/noteappmvvm/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.nebulov.noteappmvvm.feature_note.presentation.add_edit_note
+
+data class NoteTextFieldState(
+    val text: String = " ",
+    val hint: String = " ",
+    val isHintVisible: Boolean = true,
+)
